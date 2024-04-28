47e99f0a99077b09a996b0fdf4528a7c25ac4c04
Iqbal Pamula <balpamula@gmail.com>
2024-04-28 21:28:02 +0700

    feat: add navbar

---
+++ src/components/Navbar/NavbarMenu.ts
@@ -0,0 +1,20 @@
+const MenuItem = [
+  {
+    label: "Home",
+    path: "/",
+  },
+  {
+    label: "About",
+    path: "/about",
+  },
+  {
+    label: "Contact",
+    path: "/contact",
+  },
+  {
+    label: "Affiliate",
+    path: "/affiliate",
+  },
+];
+
+export default MenuItem;
